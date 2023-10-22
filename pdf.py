d371a4589f82f5159a001fde34eedb41d047a36c
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-22 23:12:50 +0530

    Introduction to PDF handling with Python

--- pdf.py
+++ pdf.py
@@ -1,5 +1,16 @@
+# PDF Merger
+# Name = Indrajeet Mondal; Date = 22th October 2023
+# SourceCode
+
 import PyPDF2
 
-with open('dummy.pdf', 'r') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    print(reader)
+with open("dummy.pdf", "rb") as file:
+    # Creating a reader object
+    reader = PyPDF2.PdfReader(file)
+    page = reader.pages[0]
+    page.rotate(270)
+    # Creating a writer object
+    writer = PyPDF2.PdfWriter()
+    writer.add_page(page)
+    with open("tilt.pdf", "wb") as new_file:
+        writer.write(new_file)
